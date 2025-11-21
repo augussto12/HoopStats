86e07b2e9cd246d046a4f9809be49890d2575f0e
pedroagueroo <aguerop47@gmail.com>
2025-11-21 16:29:32 -0300

    estilos home y mas visuales

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -18,13 +18,23 @@ export class Home implements OnInit {
   liveGames: Game[] = [];
   bestPlayers: TopStat[] = [];
 
+  menuOpen: boolean = false;
+
+
   news = [
   {
     image: 'https://ds-images.bolavip.com/news/image?src=https%3A%2F%2Fimages.bolavip.com%2Fwebp%2Fmx%2Ffull%2FBMX_20251120_BMX_390380_Lakers-despidio-a-dos-piezas-del-equipo-de-LeBron-y-compania.webp&width=490&height=275',
     title: 'Ni LeBron lo esperaba: Lakers corta a dos de las piezas más importantes del equipo en plena temporada',
     description: 'Los Angeles Lakers tomaron una decisión que nadie veía venir en la NBA cortando a dos piezas clave del equipo. ¡Ni LeBron James lo esperaba!',
     url: 'https://bolavip.com/mx/amp/nba/ni-lebron-lo-esperaba-lakers-corta-a-dos-de-las-piezas-mas-importantes-del-equipo-en-plena-temporada'
+  },
+  {
+    image: 'https://statics.ciudadano.news/2025/11/crop/691f65d262e3a__980x490.webp?v=1763665411',
+    title: 'NBA HOY: qué partidos se juegan este jueves 20 de noviembre de 2025',
+    description: 'La NBA continúa este jueves con cuatro partidos desde las 21:00 en Argentina. Revisá todos los duelos del día y dónde verlos.',
+    url: 'https://ciudadano.news/deportes/nba-hoy-partidos-juegan-jueves-20-noviembre-2025-n112090'
   }
+
 ];
 
 
